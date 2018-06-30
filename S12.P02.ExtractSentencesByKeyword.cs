19ce80f7097e674d70a7212465ac4cf934743ac8
Vasil Nikolaev Nikolov <39265096+VasilNN@users.noreply.github.com>
2018-06-30 19:57:20 +0300

    Add files via upload

---
+++ S12.P02.ExtractSentencesByKeyword.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace P02.ExtractSentencesByKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string searchedWord = Console.ReadLine();
+            string input = Console.ReadLine();
+
+            string pattern = $@"[^.!?;]*(\b{searchedWord}\b)[^.?!;]*";
+
+            Regex regex = new Regex(pattern);
+
+            var output = regex.Matches(input);
+
+            foreach (var setntece in output)
+            {
+                Console.WriteLine(setntece.ToString().Trim());
+            }
+        }
+    }
+}
