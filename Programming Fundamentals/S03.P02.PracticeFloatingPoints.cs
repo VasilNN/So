d39e8c1b49763f11a383b52c0170f2fff95f2242
Vasil Nikolaev Nikolov <39265096+VasilNN@users.noreply.github.com>
2018-08-16 15:27:14 +0300

    Create S03.P02.PracticeFloatingPoints.cs
    Adding exercise code

---
+++ Programming Fundamentals/S03.P02.PracticeFloatingPoints.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace P02.PracticeFloatingPoints
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal dataTypeDecimalOne = decimal.Parse(Console.ReadLine());
+            double dataTypeDouble = double.Parse(Console.ReadLine());
+            decimal dataTypeDecimalTwo = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine(dataTypeDecimalOne);
+            Console.WriteLine(dataTypeDouble);
+            Console.WriteLine(dataTypeDecimalTwo);
+
+        }
+    }
+}
